efca2c39ea46880c6af33a14c903a5d11fba8be8
dan1114ho <dan1114ho@gmail.com>
2019-07-13 17:17:22 -0400

    remove placeholder components

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Descriptions, message, Radio } from 'antd';
+import { Card, message } from 'antd';
 import { observable } from 'mobx';
 import { observer } from 'mobx-react';
 import Axios from 'axios';
@@ -41,37 +41,6 @@ class Demo extends React.Component <{}> {
   public render () {
     return (
       <div>
-        <Radio.Group onChange={this.onChange} value={this.state.size}>
-          <Radio value='default'>default</Radio>
-          <Radio value='middle'>middle</Radio>
-          <Radio value='small'>small</Radio>
-        </Radio.Group>
-        <br />
-        <br />
-        <Descriptions bordered title='Custom Size' border size={this.state.size}>
-          <Descriptions.Item label='Product'>Cloud Database</Descriptions.Item>
-          <Descriptions.Item label='Billing'>Prepaid</Descriptions.Item>
-          <Descriptions.Item label='time'>18:00:00</Descriptions.Item>
-          <Descriptions.Item label='Amount'>$80.00</Descriptions.Item>
-          <Descriptions.Item label='Discount'>$20.00</Descriptions.Item>
-          <Descriptions.Item label='Official'>$60.00</Descriptions.Item>
-          <Descriptions.Item label='Config Info'>
-            Data disk type: MongoDB
-            <br />
-            Database version: 3.4
-            <br />
-            Package: dds.mongo.mid
-            <br />
-            Storage space: 10 GB
-            <br />
-            Replication_factor:3
-            <br />
-            Region: East China 1<br />
-          </Descriptions.Item>
-        </Descriptions>
-
-        <br/>
-
         <Carousel />
 
         <Switch onChange={this.onSwitchChange} />
