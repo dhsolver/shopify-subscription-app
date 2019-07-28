ed8ea8240ed1b9e9859fd63ea874a86fdb4514bb
dan1114ho <dan1114ho@gmail.com>
2019-07-28 17:21:02 -0400

    switch from koa to express for API surface

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 require('isomorphic-fetch');
-const Koa = require('koa');
-const { createServer } = require('http');
-const next = require('next');
-const dotenv = require('dotenv');
-const mobxReact = require('mobx-react');
-const Client = require('shopify-buy');
-const { parse } = require('url');
-const Axios = require('axios');
+const next = require('next')
+  , dotenv = require('dotenv')
+  , express = require('express')
+  , compression = require('compression')
+  , mobxReact = require('mobx-react')
+  , Client = require('shopify-buy')
+  , Axios = require('axios')
+  ;
 
 dotenv.config();
 
-const port = parseInt(process.env.PORT, 10) || 3000;
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({dev, dir: 'src'});
-const handle = app.getRequestHandler();
+const port = parseInt(process.env.PORT, 10) || 3000
+  , dev = process.env.NODE_ENV !== 'production'
+  , app = next({dev, dir: 'src'})
+  , handle = app.getRequestHandler()
+  ;
 
 const {
   RECHARGE_API_KEY,
-  SHOPIFY_API_SECRET_KEY,
   SHOPIFY_API_KEY,
   SHOPIFY_STOREFRONT_KEY,
   SHOPIFY_DOMAIN,
@@ -49,38 +49,34 @@ const rechargeClient = Axios.create({
 mobxReact.useStaticRendering(true);
 
 app.prepare().then(() => {
-  const server = new Koa();
-  server.keys = [SHOPIFY_API_SECRET_KEY];
+  const server = express();
+  server.use(compression());
 
-  createServer(async (req, res) => {
-    // Be sure to pass `true` as the second argument to `url.parse`.
-    // This tells it to parse the query portion of the URL.
-    const parsedUrl = parse(req.url, true)
-      , { pathname, query } = parsedUrl
-      ;
+  server.get('/products/', async (req, res) => {
+    const response = await storefrontClient.product.fetchAll();
+    return res.send(JSON.stringify(response));
+  });
 
-    if (req.method === 'GET') {
-      if (pathname === '/products/') {
-        const response = await storefrontClient.product.fetchAll();
-        res.end(JSON.stringify(response));
-      }
-      else if (pathname === '/collections/with-products/') {
-        const response = await storefrontClient.collection.fetchAllWithProducts();
-        res.end(JSON.stringify(response));
-      }
-      else if (pathname === '/orders/') {
-        const response = await adminClient.get('orders.json');
-        res.end(JSON.stringify(response.data));
-      }
-      else if (pathname === '/recharge-customers/') {
-        const response = await rechargeClient.get('customers');
-        res.end(JSON.stringify(response.data));
-      }
-      else {
-        handle(req, res, parsedUrl)
-      }
-    }
-  }).listen(port, async () => {
+  server.get('/collections/with-products/', async (req, res) => {
+    const response = await storefrontClient.collection.fetchAllWithProducts();
+    return res.send(JSON.stringify(response));
+  });
+
+  server.get('/orders/', async (req, res) => {
+    const response = await adminClient.get('orders.json');
+    return res.end(JSON.stringify(response.data));
+  });
+
+  server.get('/recharge-customers/', async (req, res) => {
+    const response = await rechargeClient.get('customers');
+    return res.end(JSON.stringify(response.data));
+  });
+
+  server.get('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, async () => {
     console.log(`> Ready on http://localhost:${port}`);
   });
 });
