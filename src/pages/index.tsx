9d0511753b54bebb9c885fcaca2394df4772d076
dan1114ho <dan1114ho@gmail.com>
2019-09-15 01:43:24 -0400

    fix mount loading state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
 import autoBindMethods from 'class-autobind-decorator';
-import Link from 'next/link';
-import Button from '../components/common/Button';
-import Spacer from '../components/common/Spacer';
 import Layout from '../components/Layout';
 import URI from 'urijs';
 import Axios from 'axios';
 import Router from 'next/router';
 import { get } from 'lodash';
 import store from 'store';
+import { CircleLoader } from 'react-spinners';
+
+import { Col, Row, Spin } from 'antd';
 
 @autoBindMethods
 @observer
@@ -33,25 +33,9 @@ export default class Index extends Component <{}> {
   public render () {
     return (
       <Layout>
-        <Link href='/onboarding-name'>
-          <Button type='primary'>Get Started!</Button>
-        </Link>
-        <Spacer />
-        <Link href='/frequency-selection'>
-          <a>Quantity/Frequency Selection</a>
-        </Link>
-        <br/>
-        <Link href='/recipe-selection'>
-          <a>Recipe Selection</a>
-        </Link>
-        <br/>
-        <Link href='/checkout'>
-          <a>Checkout</a>
-        </Link>
-        <br/>
-        <Link href='/dashboard'>
-          <a>Dashboard</a>
-        </Link>
+        <Row type='flex' justify='center' align='middle'>
+          <Spin spinning indicator={<CircleLoader color='#1394C9' />} />
+        </Row>
       </Layout>
     );
   }
