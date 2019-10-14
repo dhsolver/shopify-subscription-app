9094b341b07208992372eead8b60f72d2649ab95
Ryan Palermo <rpalermodrums@users.noreply.github.com>
2019-10-13 21:13:26 -0400

    Test google analytics (#47)

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Router from 'next/router';
+import ReactGA from 'react-ga';
 import App, { Container } from 'next/app';
+
+ReactGA.initialize('UA-132685226-1');
 
 // Track client-side page views with Segment
 Router.events.on('routeChangeComplete', url => {
@@ -8,6 +11,10 @@ Router.events.on('routeChangeComplete', url => {
 });
 
 export default class MyApp extends App {
+  componentDidMount() {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }
+
   render () {
     const { Component, pageProps } = this.props
 
